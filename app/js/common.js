08dd40ddd5384dd76e416115fa4b99a5ec9026ad
Alexandr <buralex89@gmail.com>
2017-06-25 22:20:12 +0300

    first commit

---
+++ app/js/common.js
@@ -0,0 +1,23 @@
+$(function() {
+    let a = 'ddd';
+    console.log(a);
+    class Polygon {
+        constructor(height, width) {
+            this.height = height;
+            this.width = width;
+        }
+
+        get area() {
+            return this.calcArea();
+        }
+
+        calcArea() {
+            return this.height * this.width;
+        }
+    }
+
+    const square = new Polygon(10, 10);
+
+    console.log(square.area);
+
+});
